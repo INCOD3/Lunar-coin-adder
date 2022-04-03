ff18b74683e7bf7bbf756e954a73d92dff947e7a
Kiet <52035116+tuankiet2204@users.noreply.github.com>
2022-04-03 17:54:47 +0800

    first commit

---
+++ lunar_coins.py
@@ -0,0 +1,31 @@
+import os
+import xml.etree.ElementTree as ET
+
+
+# Path selection for us WSL users
+def main():
+    adder = input("Enter the Lunar coins number you want to add: ")
+    if len(adder) != 2:
+        print("Please enter a number larger than 10, exiting the program")
+        exit(-1)
+    
+    path = r"C:\Program Files (x86)\Steam\userdata\[ENTER YOUR STEAMID3 here (only the number)]\632360\remote\UserProfiles"
+    # Get all the user profile xmls
+    for filename in os.listdir(path):
+        if not (filename.endswith('.xml')): continue 
+        print("[debug] found " + filename + "profile")
+        fullname = os.path.join(path, filename)
+        tree = ET.parse(fullname)
+
+        # Add the desired amount of coins to the existing coin total
+        nCoins = tree.getroot().find("coins")                                               # Hungarian notation notes: n = node, i = int
+        iCoins = int(nCoins.text)
+        iCoins += int(adder)
+        print("[debug] added " + adder + " lunar coins to every profile")
+        nCoins.text = str(iCoins)
+        print("[debug] wrote to the xml files")
+        # Write back to the xml
+        tree.write(fullname)
+        
+if __name__ == "__main__":
+    main()
